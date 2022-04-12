c8582b4d277cfce1d15e6d5f8c74d8a0b7221783
Anastasiia <nastya.kolodezhnaya@gmail.com>
2022-04-12 21:35:59 +0300

    add downloading images

--- getwallpapers.py
+++ getwallpapers.py
@@ -2,10 +2,15 @@
 import requests
 from calendar import month_name
 from bs4 import BeautifulSoup
+import re
+
+
+def format_date_for_request(date):
+    ''' format user input data to required url format '''
+    pass
 
 
 def getwallpapers(date, img_resolution='640x480'):
-    # format user input data to url format
     month_posted = month_name[int(date[:2])].lower()
     year_posted = date[2:]
     if not month_posted == 'january':
@@ -17,21 +22,21 @@ def getwallpapers(date, img_resolution='640x480'):
 
     # request html-page from https://www.smashingmagazine.com/
     url = f'https://www.smashingmagazine.com/{year_created}/{month_created}/desktop-wallpaper-calendars-{month_posted}-{year_posted}/'
-    request = requests.get(url)
-    with open(f'smashing_{date}.html', 'w') as html_file:
-        html_file.write(request.text)
 
     # get <a> page elements (with image href) with requested resolution
-    soup = BeautifulSoup(request.text)
+    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
     img_list = soup.find_all('a', string=img_resolution)
     url_list = [item.attrs['href'] for item in img_list]
 
     # download wallpapers from each link
-    # for image_url in url_list:
-    #     img_page = requests.get(image_url)
-    #     image = img_page.find('img')
-    #     with open() as html_file:
-    #
+    for image_url in url_list:
+        img_name = re.findall(r'[^/]*$', image_url)[0]
+        request_image = requests.get(image_url)
+        if request_image.status_code == 200:
+            with open(img_name, 'wb') as file:
+                file.write(request_image.content)
+                print(f'{img_name} downloaded successfully.')
+    print(f'Success for {len(url_list)} images!')
 
 
 if __name__ == '__main__':
